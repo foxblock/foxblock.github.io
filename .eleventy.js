2ead80a5c5d55b1d1fcf609f87a2c7cc7ac5e061
foxblock <foxblock+github@gmail.com>
2025-05-28 22:06:22 +0200

    Even more debug

--- .eleventy.js
+++ .eleventy.js
@@ -289,17 +289,17 @@ module.exports = function (eleventyConfig) {
     let lastMatchPos = 0;
     for (let idx = 0; idx < str.length; ++idx) {
       if (str.substring(idx, idx+5) == "<code") {
-        console.log("DEBUG Codeblock start at", idx);
+        console.log("DEBUG Codeblock start at", idx, str.substring(idx-20, idx+20));
         isCodeBlock = true;
         idx += 5;
       }
       else if (str.substring(idx, idx+7) == "</code>") {
-        console.log("DEBUG Codeblock end at", idx);
+        console.log("DEBUG Codeblock end at", idx, str.substring(idx-20, idx+20));
         isCodeBlock = false;
         idx += 7;
       }
       else if (!isCodeBlock && str.substring(idx, idx+2) == "[[") {
-        console.log("DEBUG Link creation at", idx);
+        console.log("DEBUG Link creation at", idx, str.substring(idx-20, idx+20));
         const end = str.indexOf("]]",idx+2);
         if (end == -1)
           break; // generally should not happen, unless the markdown is broken
